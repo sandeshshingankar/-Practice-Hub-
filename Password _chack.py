29992fbb2f3ec33487821d0fccd4c1499a5e84b7
Sandesh Ashok Shingankar <sandeshshingankar8@gmail.com>
2026-01-05 21:46:57 +0530

    Add password verification functionality
    Implement password check loop until correct password is entered.

---
+++ Password _chack.py
@@ -0,0 +1,6 @@
+Correct_password = "Python@123"
+password = ""
+while password != Correct_password:
+    password = input("Enter ths password: ")
+    print("plase Enter a correct password")
+print("Access Granted")
